ee788630c9b0eb364173bb34e934e04fd76de36b
Anastasia <cookiedemons@outlook.com>
2026-04-14 17:15:25 -0400

    fix: infinite recursion

--- modules/nixos/stylix.nix
+++ modules/nixos/stylix.nix
@@ -3,12 +3,33 @@
 
     stylix = {
         enable = true;
-        image = ./wallpapers/your-image.png;
+        autoEnable = true;
+        image = ./wallpapers/stillPit.png;
         polarity = "dark";
-    
-        fonts.monospace = {
-            package = pkgs.nerd-fonts.jetbrains-mono;
-            name = "JetBrainsMono Nerd Font";
+
+        targets = {
+            gtk.enable = true;
         };
+
+        fonts = {
+            monospace = {
+                package = pkgs.nerd-fonts.jetbrains-mono;
+                name = "JetBrainsMono Nerd Font";
+            };
+            sansSerif = {
+                package = pkgs.nerd-fonts.jetbrains-mono;
+                name = "JetBrainsMono Nerd Font";
+            };
+            serif = {
+                package = pkgs.nerd-fonts.jetbrains-mono;
+                name = "JetBrainsMono Nerd Font";
+            };
+            sizes = {
+                terminal = 9;
+                applications = 9;
+                desktop = 9;
+                popups = 9;
+            };
+        }; 
     };
 }
