f6fc76c9865f25d1b2428ba4a764a616afbe3ba9
Anastasia <cookiedemons@outlook.com>
2026-04-17 15:48:37 -0400

    feat: starting to style qutebrowser with stylix colors

--- modules/home/qutebrowser/colors.nix
+++ modules/home/qutebrowser/colors.nix
@@ -1,9 +1,16 @@
-{
-    colors.webpage.darkmode = {
-        enabled = true;
-        policy = {
-            images = "never";
-            page = "always";
+{ config, lib, pkgs, ... } : let
+    colors = config.stylix.colors;
+in {
+    colors = {
+        webpage.darkmode = {
+            enabled = true;
+            policy = {
+                images = "never";
+                page = "always";
+            };
+        };
+        completion = {
+            category.border.top = "${colors.base0D}";
         };
     };
 }
