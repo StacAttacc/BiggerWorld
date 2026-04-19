f2ee658dee353e0640f7e418458be1628befcdc3
Anastasia <cookiedemons@outlook.com>
2026-04-18 21:15:35 -0400

    fix: bad import fixed, I might be blind dumb or both

--- modules/home/qutebrowser/content/default.nix
+++ modules/home/qutebrowser/content/default.nix
@@ -13,6 +13,6 @@
             user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36";
         };
         
-        user_stylesheets = ["${./user_stylesheet.css}"];
+        user_stylesheets = ["${./user_stylesheets.css}"];
     };
 }
