0e3897e3df7879a1e511979648eb9b36176f0ca3
Anastasia <cookiedemons@outlook.com>
2026-04-18 20:17:20 -0400

    feat: adding completion scrollabr settings

---
+++ modules/home/qutebrowser/completion.nix
@@ -0,0 +1,6 @@
+{
+    completion.scrollbar = lib.mkForce {
+        padding = 0;
+        width = 0;
+    };
+}
--- modules/home/qutebrowser/default.nix
+++ modules/home/qutebrowser/default.nix
@@ -1,12 +1,14 @@
 { pkgs, lib, config, ... } : let
     colors = import ./colors/default.nix { inherit config lib pkgs; };
     window = import ./window/default.nix { inherit config lib pkgs; };
+    completion = import ./completion.nix;
 in {
     programs.qutebrowser = {
         enable = true;
         settings = lib.mkMerge [
             colors
             window
+            completion
             {
                 content.user_stylesheets = ["${./user_stylesheet.css}"];
             }
