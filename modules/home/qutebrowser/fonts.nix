faa7a384ad2cba2651961df950ad2ab6d0dea6aa
Anastasia <cookiedemons@outlook.com>
2026-04-20 17:44:58 -0400

    feat: window fonts for qutebrowser

--- modules/home/qutebrowser/fonts.nix
+++ modules/home/qutebrowser/fonts.nix
@@ -2,10 +2,10 @@
     fonts = lib.mkForce {
         default_family = fontName;
         tabs = {
-            selected = "bold 11pt ${fontName}";
-            unselected = "11pt ${fontName}";
+            selected = "bold 9pt ${fontName}";
+            unselected = "9pt ${fontName}";
         };
-        statusbar = "11pt ${fontName}";
-        completion.entry = "11pt ${fontName}";
+        statusbar = "9pt ${fontName}";
+        completion.entry = "9pt ${fontName}";
     };
 }
