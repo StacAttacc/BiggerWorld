c92a45358e82e6cbfabee1d4a5b00f7387114566
Anastasia <cookiedemons@outlook.com>
2026-04-20 03:23:36 -0400

    feat: kitty rice && sudo git push

--- modules/home/kitty/default.nix
+++ modules/home/kitty/default.nix
@@ -1,12 +1,51 @@
 { pkgs, lib, config, ... } : let
     kittyLauncher = pkgs.writeShellScript "kitty-launcher" (builtins.readFile ./scripts/kitty-launcher.sh);
     kittyControl = pkgs.writeShellScript "kitty-control" (builtins.readFile ./scripts/kitty-control.sh);
+    colors = config.stylix.base16Scheme;
+    fonts = config.stylix.fonts;
 in {
     programs.kitty = {
         enable = true;
         enableGitIntegration = true;
-        settings = {
-            background_opacity = lib.mkForce "0.6";
+        settings = lib.mkForce {
+            background_opacity = "0.6";
+            color0 = "#${colors.base00}";
+            color1 = "#${colors.base04}";
+            color2 = "#${colors.base03}";
+            color3 = "#${colors.base03}";
+            color4 = "#${colors.base04}";
+            color5 = "#${colors.base04}";
+            color6 = "#${colors.base03}";
+            color7 = "#${colors.base02}";
+            
+            color8 = "#${colors.base00}";
+            color9 = "#${colors.base04}";
+            color10 = "#${colors.base03}";
+            color11 = "#${colors.base03}";
+            color12 = "#${colors.base04}";
+            color13 = "#${colors.base04}";
+            color14 = "#${colors.base03}";
+            color15 = "#${colors.base02}";
+            
+            foreground = "#${colors.base02}";
+            background = "#${colors.base00}";
+            
+            selection_foreground = "#${colors.base02}";
+            selection_background = "#${colors.base01}";
+            
+            active_tab_background = "#${colors.base02}";
+            active_tab_foreground = "#${colors.base00}";
+            inactive_tab_background = "#${colors.base02}";
+            inactive_tab_foreground = "#${colors.base02}";
+            
+            url_color = "#${colors.base03}";
+            
+            cursor = "#${colors.base02}";
+            cursor_text_color = "#${colors.base00}";
+            
+            font_family = fonts.monospace.name;
+            font_size = builtins.toString fonts.sizes.terminal;
+            
         };
     };
   
