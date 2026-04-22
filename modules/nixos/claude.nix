0f8499f296c5e168bd613b6a91ebbce8131a11b9
Anastasia <cookiedemons@outlook.com>
2026-04-22 19:28:16 -0400

    feat: claude added

--- modules/nixos/claude.nix
+++ modules/nixos/claude.nix
@@ -1,5 +1,7 @@
 { config, pkgs, inputs, ... } : {
-    nixpkgs.overlays = [ claude-code.overlays.default ];
-    nixpkgs.allowUnfreePredicate = pkg :
-        builtins.elem (pkgs.lib.getName pkg) ["claude-code"];
+    nixpkgs = {
+        overlays = [ inputs.claude-code.overlays.default ];
+        config.allowUnfreePredicate = pkg :
+            builtins.elem (pkgs.lib.getName pkg) ["claude-code"];
+    };
 }
