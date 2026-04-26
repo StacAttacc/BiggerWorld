8fa87047083a34276b0151ae9574e7da10263567
Anastasia <cookiedemons@outlook.com>
2026-04-26 02:01:05 -0400

    fix: attemp to fix screenshare

--- modules/home/hyprland/default.nix
+++ modules/home/hyprland/default.nix
@@ -19,6 +19,9 @@ in {
                 "exec-once" = [
                     "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
                 ];
+                env = [
+                    "INTEL_DEBUG,noccs"
+                ];
             }
         ];
     };
