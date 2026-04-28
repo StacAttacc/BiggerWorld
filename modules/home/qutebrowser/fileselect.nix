54d1f9fff2d45d5b7dcb975f3274f0d17894aa5b
Anastasia <cookiedemons@outlook.com>
2026-04-28 15:21:08 -0400

    fix: properly adding fileseect setting to qutebrowser

--- modules/home/qutebrowser/fileselect.nix
+++ modules/home/qutebrowser/fileselect.nix
@@ -1,5 +1,5 @@
 { ... }: {
-    content.fileselect = {
+    fileselect = {
         handler = "external";
         single_file.command = [ "kitty" "-e" "yazi" "--chooser-file={}" ];
         multiple_files.command = [ "kitty" "-e" "yazi" "--chooser-file={}" ];
