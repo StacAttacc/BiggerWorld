59cf7b95620b112b0d03a01e5746dba7f0d327be
Anastasia <cookiedemons@outlook.com>
2026-04-30 02:51:10 -0400

    feat: k3s setup

---
+++ modules/nixos/k3s-server.nix
@@ -0,0 +1,25 @@
+{ config, ... } : {
+    sops = {
+        age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+        secrets.k3s-token = {
+            sopsFile = ../../secrets/secrets.yaml;
+        };
+    };
+
+    services.k3s = {
+        enable = true;
+        role = "server";
+        tokenFile = config.sops.secrets.k3s-token.path;
+        extraFlags = "--disable traefik";
+    };
+
+    boot = {
+        kernel.sysctl."net.ipv4.ip_forward" = 1;
+        kernelModules = [ "br_netfilter"  "overlay" ];
+    };
+
+    networking.firewall = {
+        allowedTCPPorts = [ 6443 10250 ];
+        allowedUDPPorts = [ 8472 ];
+    };
+}
