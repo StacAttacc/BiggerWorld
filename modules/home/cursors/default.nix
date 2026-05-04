3fe379c02123436a2a8861841096ffc6c6141790
Anastasia <cookiedemons@outlook.com>
2026-05-04 04:30:46 -0400

    feat: cyberpunk cursors

---
+++ modules/home/cursors/default.nix
@@ -0,0 +1,17 @@
+#got the cursors from elfantasmaponi on deviantart
+#https://www.deviantart.com/elfantasmaponi/gallery
+{ pkgs, ... }:
+let
+  cyberpunk-cursors = pkgs.runCommand "cyberpunk2077-cursors" {} ''
+    mkdir -p $out/share/icons/Cyberpunk2077
+    cp -r ${./cyberpunk2077}/* $out/share/icons/Cyberpunk2077/
+  '';
+in {
+  home.pointerCursor = {
+    package = cyberpunk-cursors;
+    name = "Cyberpunk2077";
+    size = 32;
+    gtk.enable = true;
+    x11.enable = true;
+  };
+}
