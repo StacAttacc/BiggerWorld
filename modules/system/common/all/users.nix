8fdf874cdc81a06a1019dedc279ae9a641ddb3d2
Anastasia <cookiedemons@outlook.com>
2026-05-27 01:52:59 -0400

    feat: setting up steam

--- modules/system/common/all/users.nix
+++ modules/system/common/all/users.nix
@@ -11,6 +11,7 @@
             "input"
             "audio"
             "cdrom"
+            "corectrl"
         ];
     };
 
--- modules/system/common/desktops/steam.nix
+++ modules/system/common/desktops/steam.nix
@@ -1,6 +1,18 @@
 { config, pkgs, ... } : {
     programs.steam = {
         enable = true;
+        remotePlay.openFirewall = true;
+        dedicatedServer.openFirewall = true;
+        gamescopeSession.enable = true;
+        localNetworkGameTransfers.openFirewall = true;
         extraCompatPackages = [ pkgs.proton-ge-bin ];
     };
+
+    programs.gamemode.enable = true;
+    
+    environment.systemPackages = with pkgs; [
+        mangohud
+        protonup-qt
+        vulkan-tools
+    ];
 }
