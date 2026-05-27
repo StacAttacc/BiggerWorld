8e752f0877c4622904cd4b0d36c265005d666087
Anastasia <cookiedemons@outlook.com>
2026-05-26 20:15:55 -0400

    refactor: Amateus system config

---
+++ modules/system/host-specific/amateus/default.nix
@@ -0,0 +1,20 @@
+{ config, lib, pkgs, ... } : {
+    imports = [
+        ./common/all/default.nix
+        ./common/servers/k3s-agent.nix
+    ];
+
+    networking.hostName = "Amateus";
+
+    boot.loader.grub.enable = true;
+    boot.loader.grub.device = "/dev/sda";
+
+    swapDevices = [{
+        device = "/swapfile";
+       size = 8192;
+    }];
+
+    hardware.graphics = {
+        enable = true;
+    };
+}
