6e83447bbca465adb1f93bb5eb9dcdfe7ed1e4f0
anastasia <cookiedemons@outlook.com>
2026-05-28 21:49:49 -0400

    fix: removing useless corectl

--- modules/system/host-specific/antinoos/overclock.nix
+++ modules/system/host-specific/antinoos/overclock.nix
@@ -1,7 +1,4 @@
 { config, pkgs, ... } : {
-    programs.corectrl = {
-        enable = true;
-    };
     hardware.amdgpu.overdrive = { 
         gpuOverclock.enable = true;
         gpuOverclock.ppfeaturemask = "0xffffffff";
