dcfd9e9c2dc182e275b0fdbff36f2eab51b6da31
anastasia <cookiedemons@outlook.com>
2026-05-28 21:43:14 -0400

    feat: adding corectl for the gpu

--- modules/system/host-specific/antinoos/default.nix
+++ modules/system/host-specific/antinoos/default.nix
@@ -3,6 +3,7 @@
         ../../common/all/default.nix
         ../../common/desktops/default.nix
         ./graphics.nix
+        ./overclock.nix
         ./sunshine.nix
         ./storage.nix
     ];
