05696332e988d203d57feb1075c7f338c2b3d3fd
anastasia <cookiedemons@outlook.com>
2026-05-28 21:47:59 -0400

    fix: new syntax for gpu

--- modules/system/host-specific/antinoos/overclock.nix
+++ modules/system/host-specific/antinoos/overclock.nix
@@ -1,6 +1,8 @@
 { config, pkgs, ... } : {
     programs.corectrl = {
         enable = true;
+    };
+    hardware.amdgpu.overdrive = { 
         gpuOverclock.enable = true;
         gpuOverclock.ppfeaturemask = "0xffffffff";
     };
