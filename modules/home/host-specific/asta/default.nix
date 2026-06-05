02c6a4f9eb122b1b26bcb93a6c55ff5c037a099e
Anastasia <cookiedemons@outlook.com>
2026-06-05 01:50:00 -0400

    feat: better setup for yt-dlp

--- modules/home/host-specific/asta/default.nix
+++ modules/home/host-specific/asta/default.nix
@@ -1,10 +1,6 @@
 { pkgs, config, lib, inputs, ... } : {
     imports = [
         ../../common/all/default.nix
-    ];
-
-    home.packages = [
-        (pkgs.yt-dlp.override { javascriptSupport = false; })
-        pkgs.ffmpeg
+        ./yt-dlp.nix
     ];
 }
---
+++ modules/home/host-specific/asta/yt-dlp.nix
@@ -0,0 +1,18 @@
+{ pkgs, ... }: {
+    programs.yt-dlp = {
+        enable = true;
+        package = pkgs.yt-dlp.override {
+            javascriptSupport = false;
+        };
+        settings = {
+            paths = "/home/anastasia/media/fresh-downloads";
+            format = "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best";
+            merge-output-format = "mp4";
+            embed-thumbnail = true;
+            add-metadata = true;
+            audio-quality = 0;
+        };
+    };
+
+    home.packages = [ pkgs.ffmpeg ];
+}
