4f1b4ad5091a8aa216b38314c9f30f953cc2d3f1
Anastasia <cookiedemons@outlook.com>
2026-06-05 01:59:24 -0400

    feat: adding output control

--- modules/home/host-specific/asta/yt-dlp.nix
+++ modules/home/host-specific/asta/yt-dlp.nix
@@ -6,6 +6,7 @@
         };
         settings = {
             paths = "/home/anastasia/media/fresh-downloads";
+            output = "%(title)s.%(ext)s";
             format = "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best";
             merge-output-format = "mp4";
             embed-thumbnail = true;
