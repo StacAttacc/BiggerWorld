778ea0460a79b28d79a02db9a7f4c999993789b6
Anastasia <cookiedemons@outlook.com>
2026-06-13 00:40:53 -0400

    fix: fixes from crash log, explicit sync and correct output

--- modules/home/host-specific/antinoos/monitor.nix
+++ modules/home/host-specific/antinoos/monitor.nix
@@ -1,5 +1,12 @@
 { ... }: {
-    wayland.windowManager.hyprland.settings.monitor = [
-        "HDMI-A-1, preferred, auto, 1"
-    ];
+    wayland.windowManager.hyprland.settings = {
+        monitor = [
+            "HDMI-A-2, preferred, auto, 1"
+        ];
+
+        render = {
+            explicit_sync = 0;
+            explicit_sync_kms = 0;
+        };
+    };
 }
