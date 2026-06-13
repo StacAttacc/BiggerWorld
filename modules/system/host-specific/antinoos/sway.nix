8d899b0263d81cd3e19e11b8369a644161da837a
Anastasia <cookiedemons@outlook.com>
2026-06-13 03:33:44 -0400

    fix: sway crash

--- modules/system/host-specific/antinoos/sway.nix
+++ modules/system/host-specific/antinoos/sway.nix
@@ -11,6 +11,7 @@
         XDG_CURRENT_DESKTOP        = "sway";
         WLR_DRM_NO_ATOMIC          = "1";
         WLR_NO_HARDWARE_CURSORS    = "1";
+        WLR_RENDERER               = "vulkan";
     };
 
     environment.systemPackages = with pkgs; [
