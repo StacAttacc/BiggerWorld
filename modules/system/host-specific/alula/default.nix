cba637e24afd8648ea89f54b26b97289ce70e93f
Anastasia <cookiedemons@outlook.com>
2026-07-21 04:23:08 -0400

    feat: assembling system config

--- modules/system/host-specific/alula/default.nix
+++ modules/system/host-specific/alula/default.nix
@@ -5,4 +5,26 @@
         ./niri.nix
         ./overlays.nix
     ];
+
+    boot = {
+        loader = {
+            systemd-boot.enable = true;
+            efi.canTouchEfiVariables = true;
+        };
+        kernelParams = [
+            "usbcore.autosuspend=-1"
+            "intel_idle.max_cstate=1"
+            "irqpoll"
+        ];
+        blacklistedKernelModules = [
+            "axp20x_i2c"
+            "axp288_charger"
+            "axp288_fuel_gauge"
+            "axp288_adc"
+        ];
+    };
+
+    hardware.enableRedistributableFirmware = true;
+
+    networking.hostName = "Alula";
 }
