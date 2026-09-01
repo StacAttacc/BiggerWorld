9993e4f52cb1754f4728d3b3d06143c9d0b9e618
Anastasia <cookiedemons@outlook.com>
2026-09-01 01:46:57 -0400

    fix: file explorer update

--- modules/home/common/desktops/terminal/yazi.nix
+++ modules/home/common/desktops/terminal/yazi.nix
@@ -54,7 +54,7 @@
             open = {
                 rules = [
                     {
-                        name = "*/";
+                        url = "*/";
                         use = [ "edit" "open" ];
                     }
                     { 
